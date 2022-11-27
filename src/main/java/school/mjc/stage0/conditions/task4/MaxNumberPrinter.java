05d8a0c5f27cf8e321e3379d9c24d608fa758231
onisim_albarov <onisim93@gmail.com>
2022-11-27 11:02:55 +0400

    complete module4-conditions-task4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = Math.max(Math.max(first, second), third);
+        System.out.println(result);
     }
 }
